173b57727c2b800e7a04b970c7336a0df843cf48
Kaliza Esther <kalizaesther5@gmail.com>
2026-09-07 15:38:25 +0200

    updating the cors and resolved production  issues

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://rarebridge-frontend.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ],
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
